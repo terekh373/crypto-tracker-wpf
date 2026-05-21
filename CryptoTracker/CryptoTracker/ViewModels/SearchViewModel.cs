b6837b8d9ded87dc80b890221fe59d6cfb634e5d
terekh <terekh.a08>
2026-05-21 23:37:10 +0300

    feat: add SearchPage with coin search

---
+++ CryptoTracker/CryptoTracker/ViewModels/SearchViewModel.cs
@@ -0,0 +1,77 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CryptoTracker.Models;
+using CryptoTracker.Services;
+using CryptoTracker.Views;
+using System.Collections.ObjectModel;
+
+namespace CryptoTracker.ViewModels
+{
+    public partial class SearchViewModel : ObservableObject
+    {
+        private readonly CoinGeckoService _coinService;
+
+        [ObservableProperty]
+        private ObservableCollection<SearchCoin> _results = new();
+
+        [ObservableProperty]
+        private bool _isLoading = false;
+
+        [ObservableProperty]
+        private bool _hasError = false;
+
+        [ObservableProperty]
+        private string _errorMessage = string.Empty;
+
+        [ObservableProperty]
+        private bool _hasResults = false;
+
+        [ObservableProperty]
+        private string _query = string.Empty;
+
+        public SearchViewModel()
+        {
+            _coinService = ServiceLocator.CoinGeckoService;
+        }
+
+        public async Task SearchAsync(string query)
+        {
+            Query = query;
+            IsLoading = true;
+            HasError = false;
+            HasResults = false;
+            Results.Clear();
+
+            try
+            {
+                var coins = await _coinService.SearchCoinsAsync(query);
+                foreach (var coin in coins)
+                    Results.Add(coin);
+
+                HasResults = Results.Count > 0;
+            }
+            catch
+            {
+                HasError = true;
+                ErrorMessage = "Search failed. Check your internet connection and try again.";
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        [RelayCommand]
+        public void OpenCoinDetail(SearchCoin coin)
+        {
+            if (coin == null) return;
+            ServiceLocator.NavigationService.NavigateTo<CoinDetailPage>(coin.Id);
+        }
+
+        [RelayCommand]
+        public void GoBack()
+        {
+            ServiceLocator.NavigationService.GoBack();
+        }
+    }
+}
---
+++ CryptoTracker/CryptoTracker/Views/SearchPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CryptoTracker.ViewModels;
+using System.Windows.Controls;
+
+namespace CryptoTracker.Views
+{
+    public partial class SearchPage : Page
+    {
+        private readonly SearchViewModel _viewModel;
+
+        public SearchPage(string query)
+        {
+            InitializeComponent();
+            _viewModel = new SearchViewModel();
+            DataContext = _viewModel;
+            Loaded += async (s, e) => await _viewModel.SearchAsync(query);
+        }
+
+        public SearchPage() : this(string.Empty) { }
+    }
+}
